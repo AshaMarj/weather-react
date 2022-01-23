326eb493b0bab0b89deeabf65293c96e7f682411
AshaMarj <crockera2020@gmail.com>
2022-01-23 00:58:00 -0500

    Added Weather App (Skeleton)  to React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          San Miguel de Allende, <br />
+          Guanajuato, Mexico
+        </h1>
+
+        <img
+          src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+          alt="clear"
+          id="icon"
+        />
+
+        <h3> 73°C </h3>
+
+        <h4>Partly Cloudy</h4>
+
+        <h5>Wind Speed 55</h5>
+
+        <div className="Search">
+          <form>
+            <input type="Search" placeholder="Type a city" />
+            <input type="button" value="Search" />
+          </form>
+        </div>
       </header>
+      <br />
+      <footer>
+        <a href="https://github.com/AshaMarj/my-weather-app">
+          open-source code
+        </a>
+      </footer>
     </div>
   );
 }
