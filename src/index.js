56b5e0c475dcc7ed3e589759af144d9ce9f69d6c
AshaMarj <crockera2020@gmail.com>
2022-01-25 23:08:12 -0500

    Updated index.js
    Updated root element

--- src/index.js
+++ src/index.js
@@ -2,22 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Search from "./Search";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <Search />
+    </div>
+  );
+}
 
-<img
-  src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-  alt="clear"
-  id="icon"
-/>;
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
